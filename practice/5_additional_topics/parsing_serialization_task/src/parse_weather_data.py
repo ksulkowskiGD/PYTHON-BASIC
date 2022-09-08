694adf8c55e026ae3f9d83eacfb48ebcc3275390
Kamil Sulkowski <ksulkowski@griddynamics.com>
2022-09-08 18:53:20 +0200

    Add calculating weather stats for cities

--- practice/5_additional_topics/parsing_serialization_task/src/parse_weather_data.py
+++ practice/5_additional_topics/parsing_serialization_task/src/parse_weather_data.py
@@ -1,5 +1,6 @@
 import json
 import os
+import statistics as stats
 
 
 def parse_json_data(source_data_dir: str) -> dict[str, list[dict[str, float]]]:
@@ -22,9 +23,28 @@ def parse_json_data(source_data_dir: str) -> dict[str, list[dict[str, float]]]:
     return cities_weather_data
 
 
+def calculate_weather_stats(weather_data: dict[str, list[dict[str, float]]]) -> dict[str, dict[str, float]]:
+    weather_stats = {}
+    for city, raw_city_data in weather_data.items():
+        city_stats = {}
+        parsed_city_temp = [hour_entry['temp'] for hour_entry in raw_city_data]
+        parsed_city_wind_speed = [
+            hour_entry['wind_speed'] for hour_entry in raw_city_data
+        ]
+        city_stats['mean_temp'] = stats.mean(parsed_city_temp)
+        city_stats['max_temp'] = max(parsed_city_temp)
+        city_stats['min_temp'] = min(parsed_city_temp)
+        city_stats['mean_wind_speed'] = stats.mean(parsed_city_wind_speed)
+        city_stats['max_wind_speed'] = max(parsed_city_wind_speed)
+        city_stats['min_wind_speed'] = min(parsed_city_wind_speed)
+        weather_stats[city] = city_stats
+    return weather_stats
+
+
 def main():
     weather_data = parse_json_data('./source_data')
-    print(weather_data)
+    weather_stats = calculate_weather_stats(weather_data)
+    print(weather_stats)
 
 
 if __name__ == '__main__':
