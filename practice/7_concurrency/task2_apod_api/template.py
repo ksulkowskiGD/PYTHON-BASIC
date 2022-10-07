e41bff759f8d7abbc780646fbba749bb275ebbd1
Kamil Sulkowski <ksulkowski@griddynamics.com>
2022-10-07 02:25:26 +0200

    Add api_key

--- practice/7_concurrency/task2_apod_api/template.py
+++ practice/7_concurrency/task2_apod_api/template.py
@@ -1,10 +1,13 @@
-API_KEY = "YOUR_KEY"
+import requests
+
+API_KEY = "xXv0Q4G8xmc6bI0ZVpge94HBcqL3BCyzKjOtmhza"
 APOD_ENDPOINT = 'https://api.nasa.gov/planetary/apod'
 OUTPUT_IMAGES = './output'
 
 
 def get_apod_metadata(start_date: str, end_date: str, api_key: str) -> list:
-    pass
+    url: str = APOD_ENDPOINT + '?api_key=' + api_key + '&start_date=' +\
+         start_date + '&end_date=' + end_date
 
 
 def download_apod_images(metadata: list):
