32a8137b7399e6fb16aa4e346edae81f422e1a6a
Kamil Sulkowski <ksulkowski@griddynamics.com>
2022-10-07 13:01:51 +0200

    Add simple image downloading

--- practice/7_concurrency/task2_apod_api/template.py
+++ practice/7_concurrency/task2_apod_api/template.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import json
 
@@ -13,16 +14,25 @@ def get_apod_metadata(start_date: str, end_date: str, api_key: str) -> list:
 
 
 def download_apod_images(metadata: list):
-    pass
+    for day in metadata:
+        if day['media_type'] == 'image':
+            image_url: str = day['url']
+            image_name: str = image_url.split('/')[-1]
+            image: bytes = requests.get(image_url).content
+            with open(OUTPUT_IMAGES + '/' + image_name, 'wb') as fh:
+                fh.write(image)
 
 
 def main():
+    if not os.path.exists(OUTPUT_IMAGES):
+        os.makedirs(OUTPUT_IMAGES)
+
     metadata: list = get_apod_metadata(
         start_date='2021-08-01',
         end_date='2021-08-03',
         api_key=API_KEY,
     )
-    # download_apod_images(metadata=metadata)
+    download_apod_images(metadata=metadata)
 
 
 if __name__ == '__main__':
