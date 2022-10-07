675fd845832e51e13773fceb4dbe73d70a459fe2
Kamil Sulkowski <ksulkowski@griddynamics.com>
2022-10-07 12:49:14 +0200

    Add simple metadata downloading

--- practice/7_concurrency/task2_apod_api/template.py
+++ practice/7_concurrency/task2_apod_api/template.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 API_KEY = "xXv0Q4G8xmc6bI0ZVpge94HBcqL3BCyzKjOtmhza"
 APOD_ENDPOINT = 'https://api.nasa.gov/planetary/apod'
@@ -7,7 +8,8 @@ OUTPUT_IMAGES = './output'
 
 def get_apod_metadata(start_date: str, end_date: str, api_key: str) -> list:
     url: str = APOD_ENDPOINT + '?api_key=' + api_key + '&start_date=' +\
-         start_date + '&end_date=' + end_date
+        start_date + '&end_date=' + end_date
+    return json.loads(requests.get(url).content[:-1])
 
 
 def download_apod_images(metadata: list):
@@ -15,12 +17,12 @@ def download_apod_images(metadata: list):
 
 
 def main():
-    metadata = get_apod_metadata(
+    metadata: list = get_apod_metadata(
         start_date='2021-08-01',
-        end_date='2021-09-30',
+        end_date='2021-08-03',
         api_key=API_KEY,
     )
-    download_apod_images(metadata=metadata)
+    # download_apod_images(metadata=metadata)
 
 
 if __name__ == '__main__':
